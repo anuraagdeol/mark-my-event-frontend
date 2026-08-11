ad23616d324a326af666519053f3452492520830
Anurag Deol <adeol@adobe.com>
2026-08-11 14:38:33 +0530

    first commit

---
+++ src/components/EventContainer.js
@@ -0,0 +1,9 @@
+function EventContainer() {
+  return (
+    <div className="EventContainer">
+        Event Container goes here..
+    </div>
+  );
+}
+
+export default EventContainer;
---
+++ src/components/EventForm.js
@@ -0,0 +1,9 @@
+function EventForm() {
+  return (
+    <div className="EventForm">
+        Event Form goes here..
+    </div>
+  );
+}
+
+export default EventForm;
