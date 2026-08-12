74441ca1af101f3329ab51eedffe5712ca6a4ad2
Anurag Deol <adeol@adobe.com>
2026-08-12 12:25:20 +0530

    added: react form with one simple input

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+
 function EventForm() {
+  const [data, setData] = useState({
+    title: ""
+  });
+
+  useEffect(() => {
+    console.log("DEBUG:: useEffect hook called. Dependency array = []. Value of \"data\" state variable is =", data);
+  }, []);
+  useEffect(() => {
+    console.log("DEBUG:: useEffect hook called. Dependency array = [data]. Value of \"data\" state variable is =", data);
+  }, [data]);
+
+  const handleChange = (e) => {
+    setData((prevState) => {
+      return ({
+        ...prevState,
+        name: e.target.value
+      });
+    });
+  }
+
   return (
-    <div className="EventForm">
-        Event Form goes here..
-    </div>
+    <form className="EventForm">
+        <input 
+          type="text"
+          value={data?.title}
+          onChange={handleChange}
+          placeholder="Event Title"
+        />
+    </form>
   );
 }
 
